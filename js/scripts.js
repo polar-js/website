991900d363ba059b98a43cdcc3d673c11db118a3
Harry Ickeringill (Y12) <1021063@student.ccgs.wa.edu.au>
2019-11-03 23:05:39 +0800

    new api class, get latest semver

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,39 @@
-const GITHUB_CONTENT = 'https://raw.githubusercontent.com';
-const GITHUB_API = 'https://api.github.com';
+class API {
+    constructor() {
+        this.GITHUB_CONTENT = 'https://raw.githubusercontent.com';
+        this.GITHUB_API = 'https://api.github.com';
+        this.versions = this.getVersions();
+    }
+    async getVersions() {
+        const content = await fetch(this.GITHUB_API + '/repos/JellyAlex/polar.js/contents?ref=docs');
+        const json = await content.json();
+        // return ['1.1.1', '2.4.2', '5.3.1']
+        return json.map(file => file.name.replace('.json', ''));
+    };
+    
+    async getDoc(version) {
+        if (localStorage.has(version)) return localStorage.get(version);
+        const content = await fetch(this.GITHUB_CONTENT + `/JellyAlex/polar.js/docs/${version}.json`);
+        const json = await content.json();
+        localStorage.setItem(version, json);
+        return json;
+    }
 
-page('/index.html', () => console.log('abc'))
+    async getLatest() {
+        const versions = await this.versions;
+        let parsed = versions.map(ver => ver.split('.').map(v => parseInt(v)))
+        let index = 0;
+        while (parsed.length > 1) {
+            parsed = parsed.filter((ver, _, arr) => ver[index] === Math.max(...arr.map(v => v[index])))
+            index++;
+        }
+        return parsed[0].join('.');
+    }
+}
+
+const api = new API();
+
+page('/docs/stable', async () => page.redirect(`/docs/${await api.getLatest()}`));
 page('/docs/:version', (data) => console.log('opening doc vesion', data.params.version));
 page('/docs/:version/:class', (data) => console.log('opening doc vesion w class', data.params.version));
 page.start();
-
-async function getVersions() {
-    const content = await fetch(GITHUB_API + '/repos/JellyAlex/polar.js/contents?ref=docs');
-    const json = await content.json();
-    return json.map(file => ({ version: file.name.replace('.json', ''), url: file.download_url }));
-};
-
-async function getDoc(version) {
-    if (localStorage.has(version)) return localStorage.get(version);
-    const content = await fetch(GITHUB_CONTENT + `JellyAlex/polar.js/docs/${version}.json`);
-    const json = await content.json();
-    localStorage.setItem(version, json);
-    return json;
-}
