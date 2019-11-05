e2cfc95aadf6df13539fdb80bc582513a23fd429
Harry Ickeringill (Y12) <1021063@student.ccgs.wa.edu.au>
2019-11-05 22:37:14 +0800

    much better handling of caching

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,44 @@ class Documentation {
     constructor() {
         this.GITHUB_CONTENT = 'https://raw.githubusercontent.com';
         this.GITHUB_API = 'https://api.github.com';
-        this.versions = this.getVersions();
     }
 
     async getVersions() {
-        /*
-        const content = await fetch(this.GITHUB_API + '/repos/JellyAlex/polar.js/contents?ref=docs');
-        const json = await content.json();
-        return json.map(file => file.name.replace('.json', ''));
-        */
-        return ['1.1.1', '2.4.2', '5.3.1']
+        const now = new Date().getTime();
+        const lastRefresh = parseInt(localStorage.getItem('last_refresh') || 0);
+        if (lastRefresh + 3.6e+6 < now) {
+            /*
+            const content = await fetch(this.GITHUB_API + '/repos/JellyAlex/polar.js/contents?ref=docs');
+            const json = await content.json();
+            const versions = json.map(file => file.name.replace('.json', ''));
+            */
+            const versions =  ['1.1.1', '2.4.2', '5.3.1']
+            // TEMP ^
+            localStorage.setItem('last_refresh', now);
+            localStorage.setItem('versions', JSON.stringify(versions));
+            return versions;
+        };
+        return JSON.parse(localStorage.getItem('versions'));
     };
     
     async getDoc(version) {
-        const ver = JSON.parse(localStorage.getItem(version));
-        if (ver) return ver;
-        /*
-        const content = await fetch(this.GITHUB_CONTENT + `/JellyAlex/polar.js/docs/${version}.json`);
-        const json = await content.json();
-        */
-        const content = await fetch('/static/tempdocs.json');
-        const json = await content.json();
-        localStorage.setItem(version, JSON.stringify(json));
-        return json;
+        const docs = JSON.parse(localStorage.getItem('docs') || '{}');
+        console.log(docs);
+        if (!docs[version]) {
+            /*
+            const content = await fetch(this.GITHUB_CONTENT + `/JellyAlex/polar.js/docs/${version}.json`);
+            docs[version] = await content.json();
+            */
+            const content = await fetch('/static/tempdocs.json');
+            docs[version] = await content.json();
+            // TEMP ^
+            localStorage.setItem('docs', JSON.stringify(docs));
+            return docs[version];
+        }
+        return docs[version];
     }
 
-    async getLatest() {
-        const versions = await this.versions;
+    getLatest(versions) {
         let parsed = versions.map(ver => ver.split('.').map(v => parseInt(v)))
         let index = 0;
         while (parsed.length > 1) {
@@ -41,8 +52,14 @@ class Documentation {
 
 class ViewsManager {
     constructor() {
-        this.docs = new Documentation()
-        this.version('5.3.1');
+        this.docs = new Documentation();
+    }
+
+    async loadLatest() {
+        const versions = await this.docs.getVersions();
+        const latest = this.docs.getLatest(versions);
+        const doc = await this.docs.getDoc(latest);
+        this.showDoc(doc);    
     }
 
     scrollTo(view) {
@@ -53,9 +70,7 @@ class ViewsManager {
           });
     }
 
-    async version(version) {
-        const doc = await this.docs.getDoc(version);
-
+    showDoc(doc) {
         const ul = document.getElementById('classes');
         while (ul.firstChild) {
             ul.firstChild.remove();
@@ -65,14 +80,12 @@ class ViewsManager {
             .filter(c => c.kindString === 'Class')
             .forEach(c => {
                 const li = document.createElement('li');
+                li.onclick = () => console.log(c);
                 li.innerText = c.name;
                 ul.appendChild(li);
             })
     }
-
-    class(data) {
-        console.log('opening doc vesion w class', data.params.version)
-    }
 }
 
 const views = new ViewsManager()
+views.loadLatest();
